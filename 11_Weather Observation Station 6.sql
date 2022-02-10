50f8ae5789e66a30b3700648de8dee886e045b21
Siddharth Singh <singhsiddharth.1998@gmail.com>
2022-02-10 20:25:29 +0530

    Create 11_Weather Observation Station 6.sql

---
+++ 11_Weather Observation Station 6.sql
@@ -0,0 +1,14 @@
+/*Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.*/
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOU]'
+
+
+OR
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOU].*'
